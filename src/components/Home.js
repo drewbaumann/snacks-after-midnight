1454fe4a085d9050643e5eddcef8969b9646fcb9
Drew Baumann <drewbaumann@gmail.com>
2025-07-01 12:20:03 -0700

    Increase company name font size and change color to teal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-white" style={{ zIndex: 1 }}>
-      <h1 className="text-6xl" style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}>Snacks After Midnight</h1>
+      <h1 className="text-8xl text-cyan-400" style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}>Snacks After Midnight</h1>
     </div>
   );
 }
