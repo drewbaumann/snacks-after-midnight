9ad35542d80c27e5211d6e40ed5c2173b0f85bed
Drew Baumann <drewbaumann@gmail.com>
2025-07-01 12:48:24 -0700

    Update Terms of Service to refer to Snacks After Midnight

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,8 +1,133 @@
 export default function Terms() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white">
-      <h1 className="text-4xl">Terms of Service</h1>
-      <p>Coming soon...</p>
+    <div className="flex flex-col items-center justify-center h-screen text-white overflow-y-auto p-8">
+      <h1 className="text-4xl mb-8">Terms of Service</h1>
+      <div className="prose prose-invert max-w-3xl">
+        <p><em>Updated: March 29, 2024</em></p>
+
+<p><strong>These Terms of Service (“Terms”, “Terms of Service”, “Agreement”, or “Service Agreement”) govern your relationship with the services (the “Service”) operated by Snacks After Midnight (“Snacks After Midnight”, “us”, “we”, or “our”). Please read this carefully as you will be legally bound by these terms.</strong></p>
+
+<p>Your access to and use of the Service is based on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users, customers, and others who access or use the Service.</p>
+
+<p>By accessing or using the Service you agree to be bound by these Terms and accept all legal consequences. If you do not agree to the terms and conditions of this Agreement, in whole or in part, please do not use the Service.</p>
+
+<h2 id="description-of-service">Description of Service</h2>
+
+<p>The “Service” means (a) the snacksaftermidnight.com website (the “Site”), and (b) all software (including the Software, as defined below), applications, data, text, images, and other content made available by or on behalf of Snacks After Midnight. Any modifications to the Service are also subject to these Terms. Snacks After Midnight reserves the right to modify or discontinue the Service or any feature or functionality thereof at any time without notice. All rights, title and interest in and to the Service will remain with and belong exclusively to Snacks After Midnight.</p>
+
+<h2 id="subscriptions">Subscriptions</h2>
+
+<p>Some parts of the Service may be billed on a subscription basis (“Subscription(s)”). You will be billed in advance on a recurring and periodic basis (“Billing Cycle”). Billing cycles are set on a regular basis, typically monthly or yearly.</p>
+
+<p>At the end of each Billing Cycle, your Subscription will automatically renew under the same conditions unless you cancel. You may cancel your Subscription renewal through the Subscriptions section of the App Store, or by contacting Apple. Snacks After Midnight is not able to view or make adjustments to subscriptions set up through the App Store.</p>
+
+<p>All amounts paid are non-refundable. You further agree to be responsible for all taxes associated with the Service, along with any transaction fees and currency conversions added by your financial institution and intermediaries. All amounts are in US Dollars.</p>
+
+<h2 id="free-trial">Free Trial</h2>
+
+<p>Snacks After Midnight may, at its sole discretion, offer a Subscription with a free trial for a limited period of time (“Free Trial”).</p>
+
+<p>You may be required to enter your billing information in order to sign up for the Free Trial.</p>
+
+<p>If you do enter your billing information when signing up for the Free Trial, you will not be charged by Snacks After Midnight until the Free Trial has expired. On the last day of the Free Trial period, unless you canceled your Subscription, you will be automatically charged the applicable Subscription fees for the type of Subscription you have selected.</p>
+
+<p>At any time and without notice, Snacks After Midnight reserves the right to (i) modify the terms and conditions of the Free Trial offer, or (ii) cancel such Free Trial offer.</p>
+
+<h2 id="fee-changes">Fee Changes</h2>
+
+<p>Snacks After Midnight, in its sole discretion and at any time, may modify the Subscription fees for the Subscriptions. Any Subscription fee change will become effective at the end of the then-current Billing Cycle.</p>
+
+<p>Snacks After Midnight will provide you with reasonable prior notice of any change in Subscription fees to give you an opportunity to terminate your Subscription before such change becomes effective.</p>
+
+<p>Your continued use of the Service after the Subscription fee change comes into effect constitutes your agreement to pay the modified Subscription fee amount.</p>
+
+<h2 id="accounts">Accounts</h2>
+
+<p>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms.</p>
+
+<p>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third party service.</p>
+
+<p>You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+
+<p>You may not use a username of another person or entity or that is not lawfully available for use, a name or trademark that is subject to any rights of another person or entity other than you without appropriate authorization, or a name that is otherwise offensive, vulgar or obscene.</p>
+
+<h2 id="intellectual-property">Intellectual Property</h2>
+
+<p>The Service and all contents, including but not limited to text, images, graphics or code are the property of Snacks After Midnight and are protected by copyright, trademarks, database and other intellectual property rights. You may display and copy, download or print portions of the material from the different areas of the Service only for your own non-commercial use. Any other use is strictly prohibited and may violate copyright, trademark and other laws. These Terms do not grant you a license to use any trademark of Snacks After Midnight or its affiliates. You further agree not to use, change or delete any proprietary notices from materials downloaded from the Service.</p>
+
+<h2 id="user-generated-content">User-Generated Content</h2>
+
+<p>“Your Data” means any data and content which you upload, store, retrieve, or otherwise make available through the Service. You retain all of the rights to Your Data. You agree to grant Snacks After Midnight a license to store, retrieve, backup, restore, and otherwise copy Your Data so that we may provide you with the Service.</p>
+
+<h2 id="links-to-other-web-sites">Links To Other Web Sites</h2>
+
+<p>The Service may contain links to third-party web sites or services that are not owned or controlled by Snacks After Midnight.</p>
+
+<p>Snacks After Midnight has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Snacks After Midnight shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with the use of or reliance on any such content, goods or services available on or through any such websites or services.</p>
+
+<p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+
+<h2 id="termination">Termination</h2>
+
+<p>You are entitled to cease using our Services at any time and for any reason without notice to us, but you will continue to be charged for Services until you cancel your account through the App Store or by contacting Apple.</p>
+
+<p>Our duty is to keep our Service as safe and well maintained as possible. To this end, we may need to terminate accounts for violations of these Terms. In the following circumstances we will provide notice to you prior to termination of your account: (a) you are in breach of these Terms; or (b) you are using our Services in a way that can cause or has caused a risk of harm or loss to either Snacks After Midnight or other users.</p>
+
+<p>In such an event our notice will be via email or direct message to the address listed on the account at the time of notice. If you remedy the issues that caused us to send the notice, to our satisfaction, then we will not terminate your access or license to our Services. If you do not remedy the outstanding causes of our termination notice in these circumstances, then we will terminate your account.</p>
+
+<p>All provisions of the Terms shall survive termination, including without limitation: ownership provisions, warranty disclaimers, indemnity and limitations of liability. Upon termination, your right to use the Service will immediately cease.</p>
+
+<h2 id="limitation-of-liability">Limitation Of Liability</h2>
+
+<p>Snacks After Midnight, its directors, employees, partners, agents, suppliers, or affiliates, shall not be liable for (A) any loss or damage, indirect, incidental, special, consequential or punitive damages, including without limitation, economic loss, loss or damage to electronic media or data, goodwill, or other intangible losses, or (B) for any amount in the aggregate in excess of the fees actually paid by you in the six (6) months preceding the event giving rise to your claim, resulting from (i) your access to or use of the Service; (ii) your inability to access or use the Service; (iii) any conduct or content of any third-party on or related to the Service; (iv) any content obtained from or through the Service; and (v) the unauthorized access to, use of or alteration of your transmissions or content, whether based on warranty, contract, tort (including negligence) or any other claim in law, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.</p>
+
+<h2 id="disclaimer-and-non-waiver-of-rights">Disclaimer And Non-Waiver of Rights</h2>
+
+<p>Snacks After Midnight makes no guarantees, representations or warranties of any kind as regards the website and associated technology. Any purportedly applicable warranties, terms and conditions are excluded, to the fullest extent permitted by law. Your use of the Service is at your sole risk.</p>
+
+<p>The Service is provided on an “AS IS” and “AS AVAILABLE” basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance, except as provided for under the laws of the state of Colorado. In such cases, the provincial law shall apply to the extent necessary.</p>
+
+<p>Snacks After Midnight, its subsidiaries, affiliates, and its licensors do not warrant that a) the Service will function uninterrupted, secure or available at any particular time or location; b) any errors or defects will be corrected; c) the Service is free of viruses or other harmful components; or d) the results of using the Service will meet your requirements.</p>
+
+<p>If you breach any of these Terms and Snacks After Midnight chooses not to immediately act, or chooses not to act at all, Snacks After Midnight will still be entitled to all rights and remedies at any later date, or in any other situation, where you breach these Terms. Snacks After Midnight does not waive any of its rights. Snacks After Midnight shall not be responsible for any purported breach of these Terms caused by circumstances beyond its control. A person who is not a party to these Terms shall have no rights of enforcement.</p>
+
+<p>You may not assign, sub-license or otherwise transfer any of your rights under these Terms.</p>
+
+<h2 id="exclusions">Exclusions</h2>
+
+<p>As set out, above, some jurisdictions do not allow the exclusion of certain warranties or the exclusion or limitation of liability for consequential or incidental damages, so the limitations above may not apply to you.</p>
+
+<h2 id="governing-law">Governing Law</h2>
+
+<p>These Terms shall be governed by, and interpreted and enforced in accordance with, the laws in the State of Colorado and the laws of the United States, as applicable.</p>
+
+<p>If any provision of these Terms is held to be invalid or unenforceable by a court of competent jurisdiction, then any remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements, oral or otherwise, regarding the Service.</p>
+
+<h2 id="dispute-resolution">Dispute Resolution</h2>
+
+<p>All disputes and questions whatsoever which shall arise between Snacks After Midnight and you in connection with this Service Agreement, or the construction or application thereof or any provision contained in this Service Agreement or as to any act, deed or omission of any party or as to any other matter in any way relating to this Service Agreement, shall be resolved by arbitration. Such arbitration shall be conducted by a single arbitrator.</p>
+
+<p>The arbitrator shall be appointed by agreement between the parties or, in default of such agreement, such arbitrator shall be appointed by a judge overseeing the dispute, upon the application of any of the parties and such judge shall be entitled to act as such arbitrator, if he or she so desires.</p>
+
+<p>Unless otherwise agreed to by the parties, arbitration shall be held in the City of Los Angeles, California, USA. The procedure to be followed shall be agreed to by the parties or, in default of such agreement, determined by the arbitrator. The arbitrator shall have the power to proceed with the arbitration and to deliver his or her award notwithstanding the default by any party in respect of any procedural order made by the arbitrator.</p>
+
+<p>The decision arrived at by the arbitrator shall be final and binding and no appeal shall lie therefrom. Judgment upon the award rendered by the arbitrator may be entered in any court having jurisdiction.</p>
+
+<h2 id="changes">Changes</h2>
+
+<p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will make reasonable efforts to provide at least 30 days’ notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+
+<p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, in whole or in part, please stop using the website and the Service.</p>
+
+<h2 id="contact-us">Contact Us</h2>
+
+<p>If you have any questions about these Terms, you can contact us via <a href="mailto:drew@snacksaftermidnight.com">email</a> or via mail at:</p>
+
+<p>1653 7TH ST<br />
+PO BOX 481<br />
+SANTA MONICA, CA 90401<br />
+USA</p>
+      </div>
     </div>
   );
 }
