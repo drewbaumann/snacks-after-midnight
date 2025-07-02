545d5c52bba808b95a8e93b300c4098433e627b6
Drew Baumann <drewbaumann@gmail.com>
2025-07-01 18:13:27 -0700

    Implement Permanent Marker typeface for company name

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        marker: ['Permanent Marker', 'cursive'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
